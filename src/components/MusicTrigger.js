8b70633180647b7dd27ad2c31728e8e75b5f7a81
cph <stegben.benjamin@gmail.com>
2017-02-19 20:43:09 +0800

    use react-waypoint and react-howler

--- src/components/MusicTrigger.js
+++ src/components/MusicTrigger.js
@@ -1,11 +1,39 @@
 import React, { Component, PropTypes } from 'react';
+import Waypoint from 'react-waypoint';
+import ReactHowler from 'react-howler';
+
 
 class MusicTrigger extends Component {
-  render() {
-    return (
-      <div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false,
+    }
+  }
 
-      </div>
+  onEnter = () => {
+    this.setState({
+      isPlaying: true,
+    });
+  }
+
+  onLeave = () => {
+    this.setState({
+      isPlaying: false,
+    });
+  }
+
+  render() {
+    const { isPlaying } = this.state;
+    return (
+      <Waypoint onEnter={this.onEnter} onLeave={this.onLeave}>
+        <div>
+          <ReactHowler
+            src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
+            playing={isPlaying}
+          />
+        </div>
+      </Waypoint>
     );
   }
 }
