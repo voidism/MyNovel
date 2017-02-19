ac5d624d3f569e75393dccaedf34ea8addcd4b57
cph <stegben.benjamin@gmail.com>
2017-02-19 17:15:59 +0800

    router structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './App';
+import Chapter1 from './Chapter1';
+import Chapter2 from './Chapter2';
+import Chapter3GoodEnding from './Chapter3GoodEnd';
+import Chapter3BadEnding from './Chapter3BadEnd';
 import './index.css';
 
+
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="chapter1" components={Chapter1} />
+    <Route path="chapter2" components={Chapter2} />
+    <Route path="chapter3-ge" components={Chapter3GoodEnding} />
+    <Route path="chapter3-be" components={Chapter3BadEnding} />
+  </Route>
+)
+
 ReactDOM.render(
-  <App />,
+  <Router
+    routes={routes}
+    history={browserHistory}
+  />,
   document.getElementById('root')
 );
